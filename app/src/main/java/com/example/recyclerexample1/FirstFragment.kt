ccce780db2d31406490a6b526650c617183e27eb
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-08 15:01:49 +0530

    scrollable list

--- app/src/main/java/com/example/recyclerexample1/FirstFragment.kt
+++ app/src/main/java/com/example/recyclerexample1/FirstFragment.kt
@@ -1,11 +1,15 @@
 package com.example.recyclerexample1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerexample1.databinding.FragmentFirstBinding
 
@@ -24,7 +28,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        val adapter = NameAdapter(requireActivity(), getStudentNames())
+        binding.recyclerView.adapter = adapter
     }
 
     override fun onDestroyView() {
@@ -70,22 +76,25 @@ class FirstFragment : Fragment() {
         return nameList
     }
 
-    class NameAdapter(val nameList: List<String>): RecyclerView.Adapter<NameAdapter.NameHolder>() {
-        class NameHolder(private val view:View): RecyclerView.ViewHolder(view) {
-
+    class NameAdapter(private val context: Context, private val studentList: List<String>) :
+        RecyclerView.Adapter<NameAdapter.NameHolder>() {
+        class NameHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+            val textStdName: TextView = view.findViewById(R.id.textStdName)
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NameHolder {
-            TODO("Not yet implemented")
+            val layout =
+                LayoutInflater.from(context).inflate(R.layout.single_row_student, parent, false)
+            return NameHolder(layout)
         }
 
         override fun onBindViewHolder(holder: NameHolder, position: Int) {
-            TODO("Not yet implemented")
+            val name = studentList[position]
+            holder.textStdName.text = name
         }
 
         override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+            return studentList.size
         }
-
     }
 }
